d28e7a03e67ff3a7680572927c50e85d5e047ab5
eaknoppanut <eaknoppanut@hotmail.com>
2017-09-08 20:23:35 +0700

    last

--- js/index.js
+++ js/index.js
@@ -4,24 +4,24 @@ const getBeerAPIByName = (name) => fetch(`https://api.punkapi.com/v2/beers/?beer
     .catch(e => console.err('Fail'))
     .then(response => {
         let obj = response;
-        let txt = "<table style='width: 50%;margin: auto;' >"; 
-			txt += "<tr style='background-color: #FFE4B5;'>";
-			txt += "<th>" + "Image" + "</th>";
-			txt += "<th>" + "Beer name" + "</th>";
-			txt += "<th>" + "Description" + "</th>";
-			txt += "<th>" + "Yeast" + "</th>";
-			txt += "</tr>";
+        let result = "<table style='width: 50%;margin: auto;' >"; 
+			result += "<tr style='background-color: #FFFF00;'>";
+			result += "<th>" + "Image" + "</th>";
+			result += "<th>" + "Beer name" + "</th>";
+			result += "<th>" + "Description" + "</th>";
+			result += "<th>" + "Yeast" + "</th>";
+			result += "</tr>";
         for (let i = 0; i < obj.length; i++) {
-		txt += "<tr>";
-		txt += "<td>" + "<image src =" + obj[i].image_url + " style='width:50%;height:auto;'/></td>";
-        txt += "<td>" + obj[i].name + "</td>";
-        txt += "<td>" + obj[i].description + "</td>";
-		txt += "<td>" + obj[i].ingredients.yeast + "</td>";
-        txt += "</tr>";
+		result += "<tr>";
+		result += "<td>" + "<image src =" + obj[i].image_url + " style='width:50%;height:auto;'/></td>";
+        result += "<td>" + obj[i].name + "</td>";
+        result += "<td>" + obj[i].description + "</td>";
+		result += "<td>" + obj[i].ingredients.yeast + "</td>";
+        result += "</tr>";
      
         }
-		txt += "</table>";
-        document.getElementById("result").innerHTML = txt;
+		result += "</table>";
+        document.getElementById("result").innerHTML = result;
     })
     const toSearch = (name) => {
     if (name != "") {
